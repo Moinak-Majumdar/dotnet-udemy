00ff61018af094035d5418eb747448945b8f91d6
Moinak Majumdar <moinak2030@gmail.com>
2025-03-26 09:08:21 +0530

    db

---
+++ db_operations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace db_operations
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string db_uri = "Server=MOINAK05;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true";
+
+            IDbConnection dbConnection = new SqlConnection(db_uri);
+
+            DateTime rightNow = dbConnection.QuerySingle<DateTime>("Select getdate()");
+
+            Console.WriteLine(rightNow);
+        }
+    }
+}
